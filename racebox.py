449df52e3c269c8847f5572c95ad222d1c81b70c
Ian Cherrill <ian@nifty.cranmore.local>
2023-07-16 21:31:25 +0100

    footer alignmnt and button

--- racebox.py
+++ racebox.py
@@ -1,5 +1,6 @@
 #from tkinter import *
-from tkinter import ttk, PhotoImage, BOTTOM, X, Tk, Canvas
+from tkinter import (ttk, PhotoImage, Button, messagebox,
+	BOTTOM, X,Y,BOTH, Tk, Canvas, W,E,N,S, RIGHT, CENTER)
 from racebox_control import RBSignalControl
 from datetime import datetime
 
@@ -23,30 +24,37 @@ s.configure('Custom.TNotebook', tabposition='ne', background='darkgrey')
 # main screen
 n = ttk.Notebook(mainWindow, style='Custom.TNotebook',padding='0 4 0 0')
 n.pack(expand=True, fill='both') #do not add .pack to the frame creation line
-controlFrame = ttk.Frame(n, style='Control.TFrame', padding='10 10 10 10')
+signalFrame = ttk.Frame(n, style='Control.TFrame', padding='10 10 10 10')
 setupFrame = ttk.Frame(n, style='Setup.TFrame')   # second page
-n.add(controlFrame, text='Signal Control')
+n.add(signalFrame, text='Signals')
 n.add(setupFrame, text='Setup')
 
 #add widgets to control frame
-screenControl = RBSignalControl(controlFrame)
+screenControl = RBSignalControl(signalFrame)
 
 #footer
 footerFrame = ttk.Frame(mainWindow, style='Footer.TFrame')
 footerFrame.pack(side=BOTTOM, fill=X)
-footerCanvas = Canvas(footerFrame, bg="black", bd=0, width=60, height=60, highlightthickness=0)
-footerCanvas.grid(column=0, row=0)
+footerFrame.grid_columnconfigure(0, weight=1)
+footerFrame.grid_columnconfigure(1, weight=1)
+footerFrame.grid_columnconfigure(1, weight=1)
+
+logoFrame = ttk.Frame(footerFrame, style='Footer.TFrame')
+logoFrame.grid(column=0,row=0,padx=(0,0), sticky='w')
+
+footerCanvas = Canvas(logoFrame, bg="black", bd=0, width=60, height=60, highlightthickness=0)
+footerCanvas.grid(column=0,row=0,padx=(0,0))
 rotorRigLogoSmall = PhotoImage(file='sail50.png')
 footerCanvas.create_image(2,2, anchor='nw', image=rotorRigLogoSmall)
 
 rrLabel = ttk.Label(
-    footerFrame,
+    logoFrame,
     text='Rotor-Rig.com',
     foreground='darkorange',
     background='black',
     font=('Sans-Serif', 12)
 )
-rrLabel.grid(column=1,row=0,padx=(0,50))
+rrLabel.grid(column=1,row=0,padx=(0,0))
 
 timeLabel = ttk.Label(
     footerFrame,
@@ -54,8 +62,15 @@ timeLabel = ttk.Label(
     foreground='lime',
     background='black',
     font=('Monospace', 14, 'bold')
-    )
-timeLabel.grid(column=2,row=0)
+)
+timeLabel.grid(column=1,row=0,padx=(0,0), sticky=W)
+
+def __hootSound():
+	messagebox.showinfo(title='Test hoot', message='Hoot!')
+	#to do
+
+hootBtn = Button(footerFrame, text='Hoot', command=__hootSound)
+hootBtn.grid(column=2,row=0, sticky=E, padx=(0,10))
 
 def loop():
     #time
