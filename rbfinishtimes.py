e071ad975adf4dc47b8f5ee428125a035f334b6b
Ian Cherrill <ian.cherrill@rotor-rig.com>
2023-08-12 22:26:32 +0100

    initial code

--- rbfinishtimes.py
+++ rbfinishtimes.py
@@ -1,42 +1,46 @@
 from datetime import datetime
 import os
-from tkinter import BOTH, BOTTOM, CENTER, END, LEFT, NW, RIGHT, TOP, S, W, X, Y, Frame, LabelFrame, Scrollbar, ttk
+from tkinter import ALL, BOTH, BOTTOM, CENTER, LEFT, NW, RIGHT, TOP, E, W, X, Y, Canvas, Frame, Label, LabelFrame, Scrollbar, ttk
 import tkinter as tk
 from rbconfig import RaceboxConfig
 
 class FinishTimesInterface:
     def __init__(self, fControl: ttk.Frame, relay):
         self.pos = 1
+        self.finishList = []
         self.relay = relay
         self.config = RaceboxConfig()
         
         f = Frame(fControl)
         f.pack(expand=True, fill=BOTH)
         
-        #frame for text box
-        tf = Frame(f, padx=25, pady=25)
-        tf.pack(side=LEFT, fill=Y)
-        #text box
-        self.txtboxFinish = tk.Text(tf, width=40)
-        self.txtboxFinish.pack(side=LEFT, fill=Y)
-        self.txtboxFinish.tag_configure('bold', font=('monospace', 11, 'bold'))
-        #sans-serif
+        #left frame (for results)
+        lf = Frame(f, padx=10, pady=25)
+        lf.pack(side=LEFT, fill=BOTH, expand=True)
         
-        #scrollbar
-        scrollFinishBox = Scrollbar(tf)
-        scrollFinishBox.pack(side=RIGHT, fill=Y)
-        scrollFinishBox.config(command=self.txtboxFinish.yview)
-        self.txtboxFinish.config(yscrollcommand=scrollFinishBox.set)
+        canvas = Canvas(lf)
+        sb = Scrollbar(lf, orient='vertical', command=canvas.yview)
+        self.finishFrame = Frame(canvas) #do not pack this
+        cw = canvas.create_window((0, 0), window=self.finishFrame, anchor=NW)
+        canvas.configure(yscrollcommand=sb.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        sb.pack(side=RIGHT, fill=Y)        
+        self.finishFrame.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox(ALL)))
+        canvas.bind('<Configure>', lambda e: canvas.itemconfig(cw, width=e.width))
         
-        #right frame
+        #left-side internal frame for rows of times
+        self.rowFrame = Frame(self.finishFrame)
+        self.rowFrame.pack(fill=X, expand=True)
+                
+        #right frame (for the buttons etc)
         rf = Frame(f)
-        rf.pack(side=LEFT, ipadx=25, fill=Y)
+        rf.pack(side=LEFT, ipadx=10, padx=25, fill=Y)
 
         #finish button
         btnFinish = ttk.Button(rf, text="Finish", command=self.finishAction)
         btnFinish.pack(side=TOP, anchor=W, pady=50)
 
-        #right bottom frame
+        #right bottom frame (reset and save)
         rbf = LabelFrame(rf, text='Use Between Races')
         rbf.pack(side=BOTTOM, anchor=W, pady=25, ipady=25)
         #save as file button
@@ -49,13 +53,50 @@ class FinishTimesInterface:
     def finishAction(self):
         on2Off = float(self.config.get('Signals', 'finishOn2Off'))
         self.relay.onoff(on2Off)
+        self.__addFinishRow()
+        
+    def __addFinishRow(self):
+        if self.pos == 1: self.__addFinishHdrRow()
         now = datetime.now()
-        self.txtboxFinish.insert(END, '\n{:>3}'.format(self.pos), 'bold')
-        self.txtboxFinish.insert(END, '  {} '.format(now.strftime('%H:%M:%S')))
+        
+        txtPos = '{:>3}'.format(self.pos)
+        lPos = Label(self.rowFrame, text=txtPos, font='TkFixedFont')
+        lPos.grid(row=self.pos, column=0)
+        lTime = Label(self.rowFrame, text='  {} '.format(now.strftime('%H:%M:%S')), font='TkFixedFont', width=12)
+        lTime.grid(row=self.pos, column=1)
+        cbClass = ttk.Combobox(self.rowFrame, values=['Solo', 'RS200', 'Laser'], width=14)
+        cbClass.grid(row=self.pos, column=2, padx=5)
+        validateNumbers = (self.rowFrame.register(self.__onlyNumbers), '%S')
+        enSailNum = ttk.Entry(self.rowFrame, validate='key', validatecommand=validateNumbers, width=14)
+        enSailNum.grid(row=self.pos, column=3, padx=5)
+        enRaceNum = ttk.Entry(self.rowFrame, validate='key', validatecommand=validateNumbers, width=4)
+        enRaceNum.grid(row=self.pos, column=4, padx=5)
+        enRaceNum.insert(0, '1')
+        enNotes = ttk.Entry(self.rowFrame, width=40)
+        enNotes.grid(row=self.pos, column=5,padx=5)
+        #increment finish pos
         self.pos += 1
         
+    def __addFinishHdrRow(self):
+        lPos = Label(self.rowFrame, text='#')
+        lPos.grid(row=0, column=0, sticky=E)
+        lTime = Label(self.rowFrame, text='Clock')
+        lTime.grid(row=0, column=1)
+        lClass = Label(self.rowFrame, text='Class')
+        lClass.grid(row=0, column=2, sticky=W, padx=5, ipadx=2)
+        lSailNum = Label(self.rowFrame, text='Sail Number')
+        lSailNum.grid(row=0, column=3, sticky=W, padx=5, ipadx=2)
+        lRaceNum = Label(self.rowFrame, text='Race')
+        lRaceNum.grid(row=0, column=4, sticky=W, padx=5, ipadx=2)
+        lNotes = Label(self.rowFrame, text='Notes')
+        lNotes.grid(row=0, column=5, sticky=W, padx=5, ipadx=2)
+        
+    def __onlyNumbers(self, k):
+        if k in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']: return True
+        return False
+        
     def resetCounterAction(self):
-        self.txtboxFinish.delete('1.0', END)
+        ####self.txtboxFinish.delete('1.0', END)
         self.pos = 1
         
     def saveToTxtFileAction(self):
@@ -65,7 +106,7 @@ class FinishTimesInterface:
         saveFileName = self.config.get('Files','finishFileFolder') + 'finishes-{}.txt'.format(now.strftime('%Y%m%d-%H%M'))
         try:
             with open (defaultFolder + saveFileName, 'w+') as file:
-                file.write(self.txtboxFinish.get('1.0', END))
+                ####file.write(self.txtboxFinish.get('1.0', END))
                 file.close()
                 tk.messagebox.showinfo('Save File', 'File {}{} saved'.format(defaultFolder, saveFileName))
         except:
