6f86e90a2e8fdead61d6025998a219a8bd0b270a
rotor-rig <ian.cherrill@rotor-rig.com>
2023-08-23 17:50:10 +0100

    autosave completed

--- rbfinishtimes.py
+++ rbfinishtimes.py
@@ -32,8 +32,8 @@ class FinishTimesInterface:
         self.statusCodes = ['', 'RET', 'OCS', 'DSQ', 'DNF', 'Other']
                 
         #finish data
-        tmpSave = self.__getAutoSavedFinishData()
-        self.finishData = tmpSave['data']
+        asInfo = self.__getAutoSavedFinishData()
+        self.finishData = asInfo['data']
         
         self.fc = fControl
         
@@ -52,8 +52,9 @@ class FinishTimesInterface:
         lraceName = Label(raceNameFrame, text='Race name')
         lraceName.pack(side=LEFT, anchor=W)
         self.raceNameValue = StringVar()
-        self.raceNameValue.set(tmpSave['name'])
+        if len(asInfo['name']) > 0: self.raceNameValue.set(asInfo['name'])
         enRaceName = ttk.Entry(raceNameFrame, textvariable=self.raceNameValue)
+        enRaceName.bind('<KeyRelease>', lambda e: self.autoSaveAction())
         enRaceName.pack(side=LEFT, anchor=W, padx=(4,0))
         lraceTimesTitle = Label(self.raceDetailsFrame, text='Finish Times', font=FinishTimesInterface.TITLE_FONT)
         lraceTimesTitle.pack(anchor=W, pady=(2,4))
@@ -97,13 +98,13 @@ class FinishTimesInterface:
         btnReset = ttk.Button(rbf, text="Save Finishes", command=self.saveToTxtFileAction, style='Custom.TButton')
         btnReset.pack(expand=True, anchor=CENTER)          
         #reset counter button
-        btnReset = ttk.Button(rbf, text="Reset Finish Box", command=self.resetCounterAction, style='Custom.TButton')
+        btnReset = ttk.Button(rbf, text="Reset Finish Box", command=self.resetFinishBoxAction, style='Custom.TButton')
         btnReset.pack(expand=True, anchor=CENTER)
         
         #finish times header        
         self.__addFinishHdrRow()
-        if len(tmpSave['data']) > 0:
-            for row in tmpSave['data']:
+        if len(asInfo['data']) > 0:
+            for row in asInfo['data']:
                 self.__drawFinishRow(row)
                 if not row['pos'] == 0: self.pos = row['pos'] + 1
         
@@ -114,13 +115,13 @@ class FinishTimesInterface:
         self.finishData.append(finishData)
         self.pos += 1
         self.__drawFinishRow(finishData)
-        self.__autoSaveFinishData()
+        self.autoSaveAction()
         
     def nonFinishAction(self):
         finishData = self.__addFinishData(False)
         self.finishData.append(finishData)
         self.__drawFinishRow(finishData)
-        self.__autoSaveFinishData()
+        self.autoSaveAction()
                 
     def __addFinishData(self, finisher=True):
         now = datetime.now()
@@ -153,14 +154,18 @@ class FinishTimesInterface:
         
         cbClass = ttk.Combobox(self.colFrames[2], values=self.classNames, textvariable=rowData['class'], width=14)
         cbClass.pack(side=BOTTOM, pady=(above,0))
+        cbClass.bind('<KeyRelease>', lambda e: self.autoSaveAction())
+        cbClass.bind('<<ComboboxSelected>>', lambda e: self.autoSaveAction())
         
         validateNumbers = (self.colFrames[3].register(self.__onlyNumbers), '%S')
         
         enSailNum = ttk.Entry(self.colFrames[3], validate='key', validatecommand=validateNumbers, textvariable=rowData['sailnum'], width=14)
         enSailNum.pack(side=BOTTOM, pady=(above,0))
+        enSailNum.bind('<KeyRelease>', lambda e: self.autoSaveAction())
         
         enRaceNum = ttk.Entry(self.colFrames[4], validate='key', validatecommand=validateNumbers, textvariable=rowData['race'], width=4)
         enRaceNum.pack(side=BOTTOM, pady=(above,0))
+        enRaceNum.bind('<KeyRelease>', lambda e: self.autoSaveAction())
         
         if rowData['status'].get() == FinishTimesInterface.STATUS_FINISHED:
             lStatus = Label(self.colFrames[5], text=FinishTimesInterface.STATUS_FINISHED)
@@ -168,9 +173,11 @@ class FinishTimesInterface:
         else:
             cbStatus = ttk.Combobox(self.colFrames[5], values=self.statusCodes, textvariable=rowData['status'], state='readonly', width=8)
             cbStatus.pack(side=BOTTOM, pady=(above,0))
-                    
+            cbStatus.bind('<<ComboboxSelected>>', lambda e: self.autoSaveAction())
+                            
         enNotes = ttk.Entry(self.colFrames[6], textvariable=rowData['notes'], width=25)
         enNotes.pack(side=BOTTOM, pady=(above,0))
+        enNotes.bind('<KeyRelease>', lambda e: self.autoSaveAction())
 
     def __addFinishHdrRow(self):
         fileHdr = [['#', E], ['Clock', CENTER], ['Class', W], ['Sail Number', W], ['Race', W], ['Status', W], ['Notes', W]]
@@ -182,7 +189,7 @@ class FinishTimesInterface:
         if k in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']: return True
         return False
         
-    def resetCounterAction(self):
+    def resetFinishBoxAction(self):
         self.pos = 1
         self.finishData = []
         self.raceNameValue.set('')
@@ -192,9 +199,11 @@ class FinishTimesInterface:
         self.__addFinishHdrRow()
         
     def saveToTxtFileAction(self):
-        saveResult = self.saveToTxtFile()
+        saveResult = self.__saveToTxtFile()
         if saveResult['result']:
             tk.messagebox.showinfo('Save File', saveResult['msg'])
+            tmpFile = self.__getAutoSaveFileName()
+            if os.path.exists(tmpFile): os.remove(tmpFile)            
         else:
             tk.messagebox.showinfo('Save File Error', saveResult['msg'])
         
@@ -230,12 +239,15 @@ class FinishTimesInterface:
         except Exception as error:
             return {'result': False, 'msg': 'Could not save the file {}{} - {}'.format(defaultFolder, saveFileName, error)}
     
-    def __getTempFileName(self):
+    def __getAutoSaveFileName(self):
         homeFolder = os.path.expanduser('~')
-        return homeFolder + '/rbtemp.json'        
+        return homeFolder + '/rbautosave.json'
+    
+    def autoSaveAction(self):
+        self.__autoSaveFinishData()
             
     def __autoSaveFinishData(self):
-        saveFileName = self.__getTempFileName()
+        saveFileName = self.__getAutoSaveFileName()
         tempFile = {
             'name': self.raceNameValue.get(),
             'data': []
@@ -254,7 +266,6 @@ class FinishTimesInterface:
         try:
             with open (saveFileName, 'w+') as file:
                 file.write(json.dumps(tempFile))
-                #print('temp file written')
         except Exception as error:
             print(error)
             
@@ -265,7 +276,7 @@ class FinishTimesInterface:
             'name': '',
             'data': []
         }
-        tmpFile = self.__getTempFileName()
+        tmpFile = self.__getAutoSaveFileName()
         try:
             with open (tmpFile, 'r') as file:
                 tmp = json.load(file)
@@ -281,6 +292,6 @@ class FinishTimesInterface:
                             'notes': StringVar(value=row['notes'])
                         })
                 return raceInfo
-        except Exception as error:
-            #print(error)
+        except: # Exception as error:
             return raceInfo
+        
